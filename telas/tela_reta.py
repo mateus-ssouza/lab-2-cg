b7969b350fa42e464e7e6e95469e10340c3bd7e8
Simeone-Holanda <simeone.10s10@gmail.com>
2021-12-23 05:01:07 -0300

    Criado a classe que armazena os algoritmos da reta com base no combobox.

--- telas/tela_reta.py
+++ telas/tela_reta.py
@@ -1,64 +1,6 @@
-from tkinter import *
 import math
-
-def calculoPontoMedio():
-    
-    x0Aux = str(entradaX0.get())
-    y0Aux = str(entradaY0.get())
-    x1Aux = str(entradaX1.get())
-    y1Aux = str(entradaY1.get())
-    
-    if(x0Aux.find("-") != -1):
-        x0Aux = entradaX0.get().replace("-", "")
-        x0Aux = int(x0Aux) * -1
-        
-    if(y0Aux.find("-") != -1):
-        y0Aux = entradaY0.get().replace("-", "")
-        y0Aux = int(y0Aux) * -1
-    
-    if(x1Aux.find("-") != -1):
-        x1Aux = entradaX1.get().replace("-", "")
-        x1Aux = int(x1Aux) * -1
-        
-    if(y1Aux.find("-") != -1):
-        y1Aux = entradaY1.get().replace("-", "")
-        y1Aux = int(y1Aux) * -1
-    
-    x0Aux = int(x0Aux)
-    y0Aux = int(y0Aux)
-    x1Aux = int(x1Aux)
-    y1Aux = int(y1Aux)
-    
-    dx = abs(x1Aux - x0Aux)
-    dy = abs(y1Aux - y0Aux)
-    
-    p = 2 * dy - dx
-    incE = 2 * dy
-    incNE = 2 * (dy - dx)
-  
-  # Determine qual ponto final usar como posição inicial.
-    if(x0Aux > x1Aux):
-        x = x1Aux
-        y = y1Aux
-        x1Aux = x0Aux
-    else:
-        x = x0Aux
-        y = y0Aux
-    
-    img.put("black", (round(400 + x), round(400 - y)))
-    
-    while(x < x1Aux):
-        x = x + 1
-        if(p < 0):
-            print(f'P={p} X={x} Y={y}')
-            p = p + incE
-        else:
-            y = y + 1
-            print(f'P={p} X={x} Y={y}')
-            p = p + incNE
-        img.put("black", (round(400 + x), round(400 - y)))
-    
-    
+from tkinter import *
+from reta import Reta
 
     
 master = Tk()
@@ -108,31 +50,12 @@ msg_plano = Label(coordenadas_plano_cartesiano, text=f"Coordenadas do plano X:{0
 msg_plano["font"] = ("Verdana", "10", "italic", "bold")
 msg_plano.pack()
 
-lb_x0 = Label(master, text="X0:")
-lb_x0.place(x=100, y=30)
+reta = Reta()
 
-entradaX0 = Entry(master)
-entradaX0.place(x=130, y=30, width=50)
+reta.mostrar_paramentros(master,img)
+reta.combobox_algoritmos(master)
 
-lb_y0 = Label(master, text="Y0:")
-lb_y0.place(x=100, y=60)
-
-entradaY0 = Entry(master)
-entradaY0.place(x=130, y=60, width=50)
-
-lb_x1 = Label(master, text="X1:")
-lb_x1.place(x=100, y=90)
-
-entradaX1 = Entry(master)
-entradaX1.place(x=130, y=90, width=50)
-
-lb_y1 = Label(master, text="Y1:")
-lb_y1.place(x=100, y=120)
-
-entradaY1 = Entry(master)
-entradaY1.place(x=130, y=120, width=50)
-
-btnDesenhar = Button(master, text='Desenhar', command=calculoPontoMedio)
+btnDesenhar = Button(master, text='Desenhar', command=reta.execute_algoritmo)
 btnDesenhar["font"] = ("Verdana", "10", "italic", "bold")
 btnDesenhar.place(x=100, y=150)
 
