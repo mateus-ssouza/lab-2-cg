d18daaaa1d113a642dca2a46ceb0a951f3d03507
MatheusOliveiraCode <58707195+MatheusOliveiraCode@users.noreply.github.com>
2021-12-23 23:47:03 -0300

    Add files via upload

---
+++ BetaNavegation/MainMenu.py
@@ -0,0 +1,48 @@
+from tkinter import *
+
+from tela_circunferencia import *
+from tela_reta import *
+
+
+def renderReta():
+    tr = TelaReta(master)
+
+
+
+def renderCircunferencia():
+    tr = TelaCircunferencia(master)
+
+
+
+master = Tk()
+master.configure(bg="white")
+
+screen_width = 1000
+screen_height = 1000
+
+master.geometry("%dx%d+0+0" % (screen_width, screen_height))  # largura, altura, dist esquerda + dist topo
+master.wm_resizable(width=False, height=False)  # travando a tela na resolução definida
+
+widget1 = Frame(master)
+widget1.configure(bg="white")
+widget1.place(bordermode=OUTSIDE, height=100, width=500, x=300)
+label = Label(widget1,text="Projeto cg 2!", bg="white")
+label["font"] = ("Verdana", "20", "italic", "bold")
+label.pack()
+
+
+btnReta= Button(master,text="Retas",command=renderReta,bg="#3D86FC",fg="white",width=13)
+btnReta.place(x=400,y=100)
+btnReta["font"] = ("Verdana", "10", "italic", "bold")
+btnCircunferencia = Button(master,text="Circunferencias",command=renderCircunferencia,bg="#3D86FC",fg="white"
+                           ,width=13)
+btnCircunferencia.place(x=600,y=100)
+btnCircunferencia["font"]=("Verdana,10,italic,bold")
+
+
+
+
+if __name__ == "__main__":
+
+    master.mainloop()
+
---
+++ BetaNavegation/tela_reta.py
@@ -0,0 +1,100 @@
+import math
+from tkinter import *
+from reta import Reta
+
+
+
+class TelaReta:
+
+    master = None
+    screen_width = 1000
+    screen_height = 1000
+
+    canvasSizeX = 800
+    canvasSizeY = 800
+    img = None
+
+
+    def __init__(self,master):
+        self.master = Toplevel(master)
+        self.renderScreen()
+
+
+
+
+    #Tamanho da tela padrao
+
+    def renderScreen(self):
+        screen_width = 1000
+        screen_height = 1000
+
+        canvasSizeX = 800
+        canvasSizeY = 800
+
+        self.master.title('Projeto 2')
+
+        self.master.geometry("%dx%d+0+0" % (screen_width, screen_height))  # largura, altura, dist esquerda + dist topo
+        self.master.wm_resizable(width=False, height=False)  # travando a tela na resolução definida
+
+        # colocando a img de fundo e especificando a posição dela na tela
+
+        canvas = Canvas(self.master, width=canvasSizeX, height=canvasSizeY, bg="white")
+        canvas.pack(side="bottom")
+        self.canvas= canvas
+
+        # colocar o pixel como imagem
+        self.img = PhotoImage(width=screen_width, height=screen_height)
+        self.drawnScreen(canvas)
+
+
+        # criando a caixinha q mostra as coordenadas
+        widget1 = Frame(self.master)
+        widget1.place(bordermode=OUTSIDE, height=20, width=300, x=50)
+        msg = Label(widget1, text=f"Coordenadas da tela X:{0} | Y: {0}")
+        msg["font"] = ("Verdana", "10", "italic", "bold")
+        msg.pack()
+
+        # Container para mostrar todas as coordenadas do plano
+        coordenadas_plano_cartesiano = Frame(self.master)
+        coordenadas_plano_cartesiano.place(bordermode=OUTSIDE, height=20, width=400, x=450)
+        msg_plano = Label(coordenadas_plano_cartesiano, text=f"Coordenadas do plano X:{0} | Y: {0}")
+        msg_plano["font"] = ("Verdana", "10", "italic", "bold")
+        msg_plano.pack()
+
+        reta = Reta()
+
+        reta.mostrar_paramentros(self.master,self.img)
+        reta.combobox_algoritmos(self.master)
+
+        btnDesenhar = Button(self.master, text='Desenhar', command=reta.execute_algoritmo)
+        btnDesenhar["font"] = ("Verdana", "10", "italic", "bold")
+        btnDesenhar.place(x=100, y=150)
+
+        btnApagar = Button(self.master, text="Apagar", command=lambda: [self.drawnScreen(canvas)])
+
+        btnApagar["font"] = ("Verdana", "10", "italic", "bold")
+        btnApagar.place(x=800, y=150)
+
+    def drawnScreen(self,canvas):
+        # colocar o pixel como imagem
+
+
+
+        self.img.blank()
+
+        canvas.create_image((self.screen_width / 2, self.screen_height / 2), image=self.img,
+                            state="normal")
+
+
+        # criando plano cartesiano
+
+        # Linha Horizontal
+        for i in range(self.canvasSizeX):
+            self.img.put("black", (int(0 + i), int(self.canvasSizeY / 2)))
+
+        # Linha vertical
+        for i in range(self.canvasSizeY):
+            self.img.put("black", (int(self.canvasSizeX / 2), int(0 + i)))
+
+
+
